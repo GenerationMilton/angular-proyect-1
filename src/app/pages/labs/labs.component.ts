45d3254b02078b8e29f643ec2a9227abc780f558
Milton Muñoz <livemilton@gmail.com>
2024-06-22 15:43:11 -0500

    signals

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
 @Component({
   selector: 'app-labs',
   standalone: true,
@@ -14,7 +15,7 @@ export class LabsComponent {
     'Crear proyecto',
     'Crear componentes'
   ];
-  name ='Milton';
+  name =signal('Milton');
   age =37;
   disable=true;
   img='https://w3schools.com/howto/img_avatar.png';
@@ -30,7 +31,9 @@ export class LabsComponent {
   }
 
   changeHandler(event: Event){
-    console.log(event);
+    const input= event.target as HTMLInputElement;
+    const newValue= input.value;
+    this.name.set(newValue);
   }
 
   keydownHandler(event: KeyboardEvent){
