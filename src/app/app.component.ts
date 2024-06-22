f4a8db211eeca2d9e71129bcd5941c466932edd9
Milton Muñoz <livemilton@gmail.com>
2024-06-22 12:52:45 -0500

    primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+
+})
+export class AppComponent {
+  welcome = 'hola';
+  tasks=[
+    'Instalar el angular CLI',
+    'Crear proyecto',
+    'Crear componentes'
+  ];
+}
