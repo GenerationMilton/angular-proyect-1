d26fe34ad8dc056af3403c98b020f827aab02fbc
Milton Muñoz <livemilton@gmail.com>
2024-06-26 22:40:27 -0500

    controlando input and import reactiveFormsModule

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
@@ -26,6 +27,15 @@ export class LabsComponent {
     age:5,
     avatar:'https://w3schools.com/howto/img_avatar.png',
   });
+
+  //controlador input para form
+  colorCtrl = new FormControl();
+
+  constructor(){
+    this.colorCtrl.valueChanges.subscribe(value =>{
+      console.log(value);
+    })
+  }
 
   clickHandler(){
     alert('Hola')
