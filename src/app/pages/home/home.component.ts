dd530336b94593c3dbb838f800ba815f1782f4b7
Milton Muñoz <livemilton@gmail.com>
2024-07-01 11:13:26 -0500

    update new sintaxis in angular ngFor and ngIf

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,11 @@
-import { CommonModule } from '@angular/common';
+
 import { Component, Injector, computed, effect, inject, signal } from '@angular/core';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Task } from './../../models/task.model';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
